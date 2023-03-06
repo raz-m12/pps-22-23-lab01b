f7a8ce8057f3e7493ccefc53d82d8efb1df566ee
Razvan Vasile <razvanflorian.vasile@studio.unibo.it>
2023-03-06 11:36:23 +0100

    (test) knight not at pawn position

--- src/e1/E1Test.java
+++ src/e1/E1Test.java
@@ -3,19 +3,30 @@ package e1;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class E1Test {
     Logics app;
+    Pair<Integer, Integer> pawnInitialPosition = new Pair<>(3,3);;
+    Pair<Integer, Integer> knightInitialPosition = new Pair<>(1,1);;
     @BeforeEach
     void beforeEach(){
-        var pawnPos = new Pair<>(3,3);
-        var knightPos = new Pair<>(1,1);
-        app = new LogicsImpl(10, pawnPos, knightPos);
+        app = new LogicsImpl(10, pawnInitialPosition, knightInitialPosition);
     }
 
     @Test
     void knightAtStartPosition() {
-        assertTrue(app.hasKnight(1, 1));
+        var knightX = knightInitialPosition.getX();
+        var knightY = knightInitialPosition.getY();
+
+        assertTrue(app.hasKnight(knightX, knightY));
+    }
+
+    @Test
+    void knightNotAtPawnPosition() {
+        var pawnX = pawnInitialPosition.getX();
+        var pawnY = pawnInitialPosition.getY();
+
+        assertFalse(app.hasKnight(pawnX, pawnY));
     }
 }
